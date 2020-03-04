66cef9239e77ec5501cde74e23dc82f2d90646e8
Mark Stabler <mark@stablerwritingservices.com>
2020-03-04 17:06:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speaker_list)
+  speaker_list.collect {|speaker| badge_maker(speaker)}
+end
+
+
+def assign_rooms(speaker_list)
+  speaker_rooms = []
+  speaker_list.each_with_index do |speaker, index|
+    speaker_rooms.push("Hello, #{speaker}! You'll be assigned to room #{index+1}!")
+  end
+  speaker_rooms
+end
+
+def printer
+
+end
+
+# speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+# puts batch_badge_creator(speakers)
+# puts assign_rooms(speakers)
